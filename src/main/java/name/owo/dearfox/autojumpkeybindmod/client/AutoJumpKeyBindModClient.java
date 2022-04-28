984c434ff65d32a14b631fa8914fba809e2a6068
DearFox <59219907+DearFox@users.noreply.github.com>
2022-04-28 19:43:20 +0300

    22w17a 1.1.03

--- src/main/java/name/owo/dearfox/autojumpkeybindmod/client/AutoJumpKeyBindModClient.java
+++ src/main/java/name/owo/dearfox/autojumpkeybindmod/client/AutoJumpKeyBindModClient.java
@@ -14,8 +14,12 @@ import net.minecraft.client.MinecraftClient;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
 @Environment(EnvType.CLIENT)
 public class AutoJumpKeyBindModClient implements ClientModInitializer {
+    //public static final Logger LOGGER = LoggerFactory.getLogger("ModId");
     @Override
     public void onInitializeClient() {
         KeyBinding auto_jump_key_bind = KeyBindingHelper.registerKeyBinding(new KeyBinding("option.auto-jump-key-bind.key", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "option.auto-jump-key-bind.category"));
@@ -24,13 +28,20 @@ public class AutoJumpKeyBindModClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (auto_jump_key_bind.wasPressed()) {
-                MinecraftClient.getInstance().options.autoJump= !MinecraftClient.getInstance().options.autoJump;
-            };
+                //MinecraftClient.getInstance().options.autoJump= !MinecraftClient.getInstance().options.autoJump; //Старая реализация переключения авто прыжка. Не работает на снапшотах.
+                if (MinecraftClient.getInstance().options.getAutoJump().getValue()) {
+                    MinecraftClient.getInstance().options.getAutoJump().setValue(false);
+                } else {
+                    MinecraftClient.getInstance().options.getAutoJump().setValue(true);
+                }
+            }
             while (auto_jump_key_bind_on.wasPressed()) {
-                MinecraftClient.getInstance().options.autoJump=true;
-            };
+                //MinecraftClient.getInstance().options.autoJump=true;
+                MinecraftClient.getInstance().options.getAutoJump().setValue(true);
+            }
             while (auto_jump_key_bind_off.wasPressed()) {
-                MinecraftClient.getInstance().options.autoJump=false;
+                //MinecraftClient.getInstance().options.autoJump=false;
+                MinecraftClient.getInstance().options.getAutoJump().setValue(false);
             }
         });
     }
