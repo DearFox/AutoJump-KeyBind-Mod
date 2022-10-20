73defadf0ce7c360bcc2f4e0504205a1d42e8eb8
DearFox <59219907+DearFox@users.noreply.github.com>
2022-10-20 10:00:42 +0300

    AutoJump KeyBind fabric 1.19.X (1.1.07)
    For the tests, one of the 1.19 snapshots is used
    But fully compatible with 1.19.2

--- src/main/java/name/owo/dearfox/autojumpkeybindmod/client/AutoJumpKeyBindModClient.java
+++ src/main/java/name/owo/dearfox/autojumpkeybindmod/client/AutoJumpKeyBindModClient.java
@@ -1,9 +1,5 @@
 package name.owo.dearfox.autojumpkeybindmod.client;
 
-import net.minecraft.text.KeybindText;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.lwjgl.glfw.GLFW;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -11,16 +7,21 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.LiteralTextContent;
+import net.minecraft.text.Text;
+
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.MinecraftClient;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
-import java.util.logging.Logger;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
 public class AutoJumpKeyBindModClient implements ClientModInitializer {
+    //public static final Logger LOGGER = LoggerFactory.getLogger("ModId");
     @Override
     public void onInitializeClient() {
         KeyBinding auto_jump_key_bind = KeyBindingHelper.registerKeyBinding(new KeyBinding("option.auto-jump-key-bind.key", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "option.auto-jump-key-bind.category"));
@@ -29,20 +30,29 @@ public class AutoJumpKeyBindModClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (auto_jump_key_bind.wasPressed()) {
-                MinecraftClient.getInstance().options.autoJump = !MinecraftClient.getInstance().options.autoJump;
-                assert client.player != null;
-                client.player.sendMessage(new LiteralText(new TranslatableText("options.autoJump").getString() + ": " + new TranslatableText((MinecraftClient.getInstance().options.autoJump)? "options.on":"options.off").getString()), true);
-                    }
-                    while (auto_jump_key_bind_on.wasPressed()) {
-                        MinecraftClient.getInstance().options.autoJump = true;
-                        assert client.player != null;
-                        client.player.sendMessage(new LiteralText(new TranslatableText("options.autoJump").getString() + ": " + new TranslatableText("options.on").getString()), true);
-                    }
-                    while (auto_jump_key_bind_off.wasPressed()) {
-                        MinecraftClient.getInstance().options.autoJump = false;
-                        assert client.player != null;
-                        client.player.sendMessage(new LiteralText(new TranslatableText("options.autoJump").getString() + ": " + new TranslatableText("options.off").getString()), true);
-                    }
-                });
+                //MinecraftClient.getInstance().options.autoJump= !MinecraftClient.getInstance().options.autoJump; //Старая реализация переключения авто прыжка. Не работает на снапшотах.
+                if (MinecraftClient.getInstance().options.getAutoJump().getValue()) {
+                    MinecraftClient.getInstance().options.getAutoJump().setValue(false);
+                    assert client.player != null;
+                    client.player.sendMessage(Text.of(new LiteralTextContent(Text.translatable("options.autoJump").getString() + ": " + Text.translatable("options.off").getString()).string()), true);
+                } else {
+                    MinecraftClient.getInstance().options.getAutoJump().setValue(true);
+                    assert client.player != null;
+                    client.player.sendMessage(Text.of(new LiteralTextContent(Text.translatable("options.autoJump").getString() + ": " + Text.translatable("options.on").getString()).string()), true);
+                }
             }
-        }
+            while (auto_jump_key_bind_on.wasPressed()) {
+                //MinecraftClient.getInstance().options.autoJump=true;
+                MinecraftClient.getInstance().options.getAutoJump().setValue(true);
+                assert client.player != null;
+                client.player.sendMessage(Text.of(new LiteralTextContent(Text.translatable("options.autoJump").getString() + ": " + Text.translatable("options.on").getString()).string()), true);
+            }
+            while (auto_jump_key_bind_off.wasPressed()) {
+                //MinecraftClient.getInstance().options.autoJump=false;
+                MinecraftClient.getInstance().options.getAutoJump().setValue(false);
+                assert client.player != null;
+                client.player.sendMessage(Text.of(new LiteralTextContent(Text.translatable("options.autoJump").getString() + ": " + Text.translatable("options.off").getString()).string()), true);
+            }
+        });
+    }
+}
